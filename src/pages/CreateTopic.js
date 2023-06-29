23064c5e09d89535a99e312acfbd7c4b22439ca5
Daniel Gonçalves <danielgoncalves229@gmail.com>
2023-06-29 18:15:36 +0100

    Add logic to createtopic page

--- src/pages/CreateTopic.js
+++ src/pages/CreateTopic.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useRef, useState, useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import Header from "./Header.js";
 import styles from "./createTopic.module.css";
 import Grid from "@mui/material/Unstable_Grid2";
@@ -7,7 +7,108 @@ import Grid from "@mui/material/Unstable_Grid2";
 import god from "../img/godofwar.png";
 
 export default function CreateTopic() {
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+  const [image, setImage] = useState("");
+  const [releaseDate, setReleaseDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [searchParams] = useSearchParams();
+  const errRef = useRef();
+  const [errMsg, setErrMsg] = useState("");
   const navigate = useNavigate();
+
+  function removeHTMLTags(text) {
+    const tempElement = document.createElement("div");
+    tempElement.innerHTML = text;
+    return tempElement.textContent || tempElement.innerText || "";
+  }
+
+  const handleFormOnSubmit = async (e) => {
+    e.preventDefault();
+    const forum_id = searchParams.get("game_id");
+    const user_id = localStorage.getItem("id");
+    if (!forum_id || !user_id) {
+      navigate("/homepage");
+    }
+    if (!name || !text) {
+      setErrMsg("Fill in all fields");
+      return;
+    }
+    setErrMsg("");
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:3000/topic/create", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = () => {
+      const data = JSON.parse(xhr.responseText);
+      if (xhr.status === 201) {
+        if (data.status == 200) {
+          navigate(-1);
+        }
+      } else {
+        console.error("Request failed. Status:", xhr.status);
+      }
+    };
+
+    xhr.onerror = () => {
+      console.error("Request failed. Network error.");
+    };
+
+    const jsonData = {
+      name,
+      text,
+      user_id,
+      forum_id,
+    };
+
+    const payload = JSON.stringify(jsonData);
+    xhr.send(payload);
+  };
+
+  function Error() {
+    if (errMsg) {
+      return (
+        <p
+          ref={errRef}
+          className={errMsg ? "errmsg" : "offscreen"}
+          aria-live="assertive"
+        >
+          {errMsg}
+        </p>
+      );
+    } else return;
+  }
+
+  useEffect(() => {
+    const id = searchParams.get("game_id");
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:3000/game/searchbyid", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = () => {
+      const data = JSON.parse(xhr.responseText);
+      if (xhr.status === 201) {
+        if (data.status == 200) {
+          setImage(data.message.image);
+          setReleaseDate(data.message.release_date);
+          setDescription(removeHTMLTags(data.message.description));
+        }
+      } else {
+        console.error("Request failed. Status:", xhr.status);
+      }
+    };
+
+    xhr.onerror = () => {
+      console.error("Request failed. Network error.");
+    };
+
+    const jsonData = {
+      id,
+    };
+
+    const payload = JSON.stringify(jsonData);
+    xhr.send(payload);
+  }, []);
 
   return (
     <>
@@ -16,13 +117,16 @@ export default function CreateTopic() {
         <Grid container spacing={10} className={styles.createTopic}>
           <Grid xs={12} sm={6}>
             <h1>New Topic</h1>
-            <form>
+            <Error />
+            <form onSubmit={handleFormOnSubmit}>
               <div>
                 <h3>Topic Title</h3>
                 <input
                   placeholder="Write your topic title"
                   type="text"
+                  autoComplete="off"
                   id={styles.text}
+                  onChange={(e) => setName(e.target.value)}
                 ></input>
               </div>
               <div>
@@ -30,18 +134,28 @@ export default function CreateTopic() {
                 <textarea
                   placeholder="Say something..."
                   id={styles.textarea}
+                  onChange={(e) => setText(e.target.value)}
                 ></textarea>
+              </div>
+              <div id={styles.submit}>
+                <input type="submit" value="Create Topic"></input>
               </div>
             </form>
           </Grid>
-          <Grid sm={6} sx={{ display: { xs: "none", sm: "block" } }}>
+          <Grid
+            sm={6}
+            sx={{ display: { xs: "none", sm: "block" } }}
+            style={{ maxHeight: "200px" }}
+          >
             <h1>The Last of Us Part II</h1>
             <div className={styles.game}>
-              <img src={god} id={styles.game}></img>
+              <img src={image} id={styles.game}></img>
               <label>
-                <b>Released: </b>09/11/2022
+                <b>Released: </b>
+                {releaseDate}
               </label>
             </div>
+            <p>{description}</p>
           </Grid>
         </Grid>
       </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,107 +4,139 @@ import "./login.css";
 import img from "../img/logo.png";
 
 export default function Login() {
+  const userRef = useRef();
+  const errRef = useRef();
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const navigate = useNavigate();
 
-    const userRef = useRef();
-    const errRef = useRef();
-    const [username,setusername] = useState('');
-    const[password,setpassword] = useState('');
-    const [errMsg,setErrMsg] = useState('');
-    const navigate = useNavigate();
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
 
-    useEffect(() => {
-      userRef.current.focus();
-    }, []);
+  useEffect(() => {
+    setErrMsg("");
+  }, [username, setusername]);
 
-    useEffect(() => {
-      setErrMsg("");
-      }, [username, setusername]);
+  const handleSubmitlogin = async (e) => {
+    e.preventDefault();
+    if (!username || !password) {
+      setErrMsg("Fill in all fields");
+      return;
+    }
+    setErrMsg("");
+    console.log(username, password);
 
-    const handleSubmitlogin = async (e) => {
-      e.preventDefault();
-      if (!username || !password) {
-          setErrMsg("Fill in all fields");
-          return;
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:3000/user/login", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = () => {
+      const data = JSON.parse(xhr.responseText);
+      if (xhr.status === 201) {
+        console.log(data);
+        if (data.status == 200) {
+          localStorage.setItem("id", data.id);
+          localStorage.setItem("user", data.username);
+          navigate("/homepage");
+          setErrMsg("");
+        } else {
+          setErrMsg(data.message);
         }
-      setErrMsg('');
-      console.log(username,password);
-
-      const xhr = new XMLHttpRequest();
-      xhr.open("POST", "http://localhost:3000/user/login", true);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.onload = () => {
-          const data = JSON.parse(xhr.responseText);
-          if (xhr.status === 201) {
-              console.log(data)
-              if (data.status == 200){
-                localStorage.setItem("user",data);
-                navigate('/homepage')
-                setErrMsg('');
-              }
-              else{
-                setErrMsg(data.message);
-              }
-            
-            } else {
-              console.error("Request failed. Status:", xhr.status);
-            }
-      };
-      xhr.onerror = () => {
-          console.error("Request failed. Network error.");
-        };
-        const jsonData = {
-          username,
-          password
-        };
-        
-      const payload = JSON.stringify(jsonData);
-      xhr.send(payload);
-      setusername("");
-      setpassword("");
+      } else {
+        console.error("Request failed. Status:", xhr.status);
+      }
+    };
+    xhr.onerror = () => {
+      console.error("Request failed. Network error.");
+    };
+    const jsonData = {
+      username,
+      password,
     };
 
-    //localStorage.setItem("user",data);
-    // navigate('/userDetails');
-    // setloginauto(true);
-    // if(loginauto==true)
-    //{
-    //   localStorage.setItem("token",data.data);
-    // }
+    const payload = JSON.stringify(jsonData);
+    xhr.send(payload);
+    setusername("");
+    setpassword("");
+  };
 
-    const registerpage = () =>{
-        navigate('/register');
-    }
+  //localStorage.setItem("user",data);
+  // navigate('/userDetails');
+  // setloginauto(true);
+  // if(loginauto==true)
+  //{
+  //   localStorage.setItem("token",data.data);
+  // }
 
-    const forgotpasswordpage = () =>{
-        navigate('/forgotpassword');
-    }
+  const registerpage = () => {
+    navigate("/register");
+  };
+
+  const forgotpasswordpage = () => {
+    navigate("/forgotpassword");
+  };
 
   return (
-    <>   
-    <div className="App">
-      <div className="App-main">
-        <div className='firstdiv'>
-            <img src={img} alt="Logo GameState" className='logo'></img>
-            <p >Login to access a lot of game reviews and get to make your OWN!</p>
-        </div>
-        <div className="login">
-            <p ref={errRef} className={errMsg?"errmsg":"offscreen"} aria-live="assertive">{errMsg}</p>
-            <form  onSubmit={handleSubmitlogin}>
-                <div className='edittext'>
-                    <label><b>Username</b></label>   
-                    <input type="text" name="username" id="username" placeholder="Username" autocomplete="off" ref={userRef}  onChange={(e)=>setusername(e.target.value)} value={username}/>
-                </div>
-                <div className='edittext'>
-                    <label><b>Password</b></label>    
-                    <input type="password" name="password" id="password" placeholder="Password" autocomplete="off"  onChange={(e)=>setpassword(e.target.value)} value={password}/>
-                </div>
-                <div className='newgamestate'>
-                    <p className="forgetp">Forgot your password? Reset it <a onClick={forgotpasswordpage}>here</a></p>
-                </div>
-                <div className='editbutton'>
-                    <input type="submit" name="log" id="log" value="Login" />   
-                    <p className='registerbutton'>New to GameState? <a onClick={registerpage}>Click here</a></p>
-                </div>
+    <>
+      <div className="App">
+        <div className="App-main">
+          <div className="firstdiv">
+            <img src={img} alt="Logo GameState" className="logo"></img>
+            <p>
+              Login to access a lot of game reviews and get to make your OWN!
+            </p>
+          </div>
+          <div className="login">
+            <p
+              ref={errRef}
+              className={errMsg ? "errmsg" : "offscreen"}
+              aria-live="assertive"
+            >
+              {errMsg}
+            </p>
+            <form onSubmit={handleSubmitlogin}>
+              <div className="edittext">
+                <label>
+                  <b>Username</b>
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Username"
+                  autocomplete="off"
+                  ref={userRef}
+                  onChange={(e) => setusername(e.target.value)}
+                  value={username}
+                />
+              </div>
+              <div className="edittext">
+                <label>
+                  <b>Password</b>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  autocomplete="off"
+                  onChange={(e) => setpassword(e.target.value)}
+                  value={password}
+                />
+              </div>
+              <div className="newgamestate">
+                <p className="forgetp">
+                  Forgot your password? Reset it{" "}
+                  <a onClick={forgotpasswordpage}>here</a>
+                </p>
+              </div>
+              <div className="editbutton">
+                <input type="submit" name="log" id="log" value="Login" />
+                <p className="registerbutton">
+                  New to GameState? <a onClick={registerpage}>Click here</a>
+                </p>
+              </div>
             </form>
           </div>
         </div>
