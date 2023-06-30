b5db03683bd6bae6e9d9f748be6f51ede38c58dc
Daniel Gonçalves <danielgoncalves229@gmail.com>
2023-06-30 23:05:47 +0100

    Change createTopic design

--- src/pages/CreateTopic.js
+++ src/pages/CreateTopic.js
@@ -1,33 +1,29 @@
 import { useRef, useState, useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import Header from "./Header.js";
-import styles from "./createTopic.module.css";
-import Grid from "@mui/material/Unstable_Grid2";
-
-import god from "../img/godofwar.png";
+import "./createTopic.css";
 
 export default function CreateTopic() {
   const [name, setName] = useState("");
   const [text, setText] = useState("");
   const [image, setImage] = useState("");
+  const [backgroundImage, setBackgroundImage] = useState("");
   const [releaseDate, setReleaseDate] = useState("");
-  const [description, setDescription] = useState("");
+  const [developer, setDeveloper] = useState("");
+  const [gameName, setGameName] = useState("");
+
   const [searchParams] = useSearchParams();
   const errRef = useRef();
   const [errMsg, setErrMsg] = useState("");
-  const navigate = useNavigate();
 
-  function removeHTMLTags(text) {
-    const tempElement = document.createElement("div");
-    tempElement.innerHTML = text;
-    return tempElement.textContent || tempElement.innerText || "";
-  }
+  const navigate = useNavigate();
 
   const handleFormOnSubmit = async (e) => {
     e.preventDefault();
     const forum_id = searchParams.get("game_id");
     const user_id = localStorage.getItem("id");
     if (!forum_id || !user_id) {
+      alert("Unknown error");
       navigate("/homepage");
     }
     if (!name || !text) {
@@ -42,7 +38,7 @@ export default function CreateTopic() {
     xhr.onload = () => {
       const data = JSON.parse(xhr.responseText);
       if (xhr.status === 201) {
-        if (data.status == 200) {
+        if (data.status === 200) {
           navigate(-1);
         }
       } else {
@@ -88,10 +84,12 @@ export default function CreateTopic() {
     xhr.onload = () => {
       const data = JSON.parse(xhr.responseText);
       if (xhr.status === 201) {
-        if (data.status == 200) {
+        if (data.status === 200) {
+          setGameName(data.message.name);
+          setDeveloper(data.message.developers[0].name);
           setImage(data.message.image);
+          setBackgroundImage(data.message.imageadd);
           setReleaseDate(data.message.release_date);
-          setDescription(removeHTMLTags(data.message.description));
         }
       } else {
         console.error("Request failed. Status:", xhr.status);
@@ -114,50 +112,61 @@ export default function CreateTopic() {
     <>
       <Header></Header>
       <div className="Appprinicipal">
-        <Grid container spacing={10} className={styles.createTopic}>
-          <Grid xs={12} sm={6}>
-            <h1>New Topic</h1>
-            <Error />
-            <form onSubmit={handleFormOnSubmit}>
-              <div>
-                <h3>Topic Title</h3>
-                <input
-                  placeholder="Write your topic title"
-                  type="text"
-                  autoComplete="off"
-                  id={styles.text}
-                  onChange={(e) => setName(e.target.value)}
-                ></input>
-              </div>
-              <div>
-                <h3>Topic Text</h3>
-                <textarea
-                  placeholder="Say something..."
-                  id={styles.textarea}
-                  onChange={(e) => setText(e.target.value)}
-                ></textarea>
-              </div>
-              <div id={styles.submit}>
-                <input type="submit" value="Create Topic"></input>
-              </div>
-            </form>
-          </Grid>
-          <Grid
-            sm={6}
-            sx={{ display: { xs: "none", sm: "block" } }}
-            style={{ maxHeight: "200px" }}
-          >
-            <h1>The Last of Us Part II</h1>
-            <div className={styles.game}>
-              <img src={image} id={styles.game}></img>
-              <label>
-                <b>Released: </b>
-                {releaseDate}
-              </label>
+        <div className="col-lg-3">
+          <div class="img-container">
+            <div className="positioning">
+              <h1 className="h2title">{gameName}</h1>
+              <p className="texttitle">
+                Released on <span className="textdata">{releaseDate} </span> by{" "}
+                <span className="texttitle1">{developer} </span>
+              </p>
             </div>
-            <p>{description}</p>
-          </Grid>
-        </Grid>
+            <div className="positionimggame">
+              <img
+                src={image}
+                className="imggame"
+                width="330px"
+                height="330px"
+                alt="Logo Jogo"
+              ></img>
+            </div>
+            <div className="imagebackground">
+              <img
+                alt="Background"
+                src={backgroundImage}
+                height="550px"
+                className="gameimgback"
+              ></img>
+            </div>
+          </div>
+        </div>
+        <div className="createTopic">
+          <h1>New Topic</h1>
+          <Error />
+          <form onSubmit={handleFormOnSubmit}>
+            <div>
+              <h3>Topic Title</h3>
+              <input
+                placeholder="Write your topic title"
+                type="text"
+                autoComplete="off"
+                id="text"
+                onChange={(e) => setName(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <h3>Topic Text</h3>
+              <textarea
+                placeholder="Say something..."
+                id="textarea"
+                onChange={(e) => setText(e.target.value)}
+              ></textarea>
+            </div>
+            <div id="submit">
+              <input type="submit" value="Create Topic"></input>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
